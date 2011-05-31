d716d4e791bb6ed19ec13a8576b024edf9f586c4
Alexander Kirk <alexander@kirk.at>
2011-05-31 15:06:21 +0200

    inofficial documentation

--- explorer.php
+++ explorer.php
@@ -4,11 +4,6 @@
 	<meta http-equiv="Content-type" content="text/html; charset=utf-8">
 <style type="text/css" media="screen">
 /* <![CDATA[ */
-h3 {
-	border-top: 1px solid #ccc;
-	margin-top: 2em;
-	padding-top: 2em;
-}
 pre {
 	border: 1px solid #999;
 	background-color: #eee;
@@ -47,11 +42,24 @@ function httpPost($url, $data) {
 	)));
 }
 
+function xmlHighlight($s) {
+	$s = preg_replace("|<(/?)([^>]+)>|", "[1]<\\1[2]\\2[/2]>[/1]", $s);
+	$s = preg_replace("|(\s)([^= ]+)\=\"([^\"]*)\"|", "\\1[3]\\2[/3][6]=[/6][4]\"\\3\"[/4]",$s);
+	$s = htmlspecialchars($s);
+	$replace = array(1 => '0000FF', 2 => '0000FF', 3 => '800000', 4 => 'FF00FF', 5 => 'FF0000', 6 => '0000FF');
+	foreach ($replace as $k => $v) {
+		$s = preg_replace("|\[" . $k . "\]|", "<font color=\"#" . $v . "\">", $s);
+	}
+	$s = preg_replace("|\[/[1-6]]|", "</font>", $s);
+	return $s;
+}
+
 
 $apis = array(
 	"api_search_location_stops_nearby" => array("outputCoords" => "WGS84", "fromCoordName" => "WGS84", "fromType" => "coords", "fromWgs84Lat" => "48.22", "fromWgs84Lon" => "16.39"),
-	"api_get_monitor" => array("monitor" => array("outputCoords" => "WGS84", "type" => "stop", "name" => "60201040", "year" => "2011", "month" => "05", "day" => "30", "hour" => "16", "minute" => "04", "line" => "", "sourceFrom" => "stoplist")),
-	"api_get_route" => array("outputCoords" => "WGS84", "from" => "Aktueller Standort", "fromType" => "coords", "fromCoordName" => "WGS84", "fromWgs84Lat" => "48.22", "fromWgs84Lon" => "16.39", "to" => "60201040", "toType" => "stop", "year" => "2011", "month" => "05", "day" => "30", "hour" => "16", "minute" => "04", "deparr" => "dep", "modality" => "pt", "sourceFrom" => "gps", "sourceTo" => "stoplist"),
+	"api_search_location" => array("outputCoords" => "WGS84", "from" => "Praterstern"),
+	"api_get_monitor" => array("monitor" => array("outputCoords" => "WGS84", "type" => "stop", "name" => "60201040", "year" => date("Y"), "month" => date("m"), "day" => date("d"), "hour" => date("H"), "minute" => date("i"), "line" => "", "sourceFrom" => "stoplist")),
+	"api_get_route" => array("outputCoords" => "WGS84", "from" => "Aktueller Standort", "fromType" => "coords", "fromCoordName" => "WGS84", "fromWgs84Lat" => "48.22", "fromWgs84Lon" => "16.39", "to" => "60201040", "toType" => "stop", "year" => date("Y"), "month" => date("m"), "day" => date("d"), "hour" => date("H"), "minute" => date("i"), "deparr" => "dep", "modality" => "pt", "sourceFrom" => "gps", "sourceTo" => "stoplist"),
 );
 if (!isset($_GET["api"])) $_GET["api"] = reset(array_keys($apis));
 
@@ -61,18 +69,18 @@ if (isset($apis[$_GET["api"]]) && isset($_GET[$_GET["api"] . reset(array_keys($a
 		$params[$key] = $_GET[$_GET["api"] . $key];
 	}
 	$req = buildRequest($_GET["api"], $params);
-	echo "<pre>" . htmlspecialchars($req) . "</pre>";
+	echo "POST to <tt>http://webservice.qando.at/2.0/webservice.ft</tt><pre>" . xmlHighlight($req) . "</pre>";
 	$response = httpPost("http://webservice.qando.at/2.0/webservice.ft", $req);
 	
 	$dom = new DOMDocument();
 	$dom->formatOutput = true;
 	$dom->preserveWhiteSpace = false;
 	$dom->loadXML($response);
-	echo "<pre>" . htmlspecialchars($dom->saveXML()) . "</pre>";
+	echo "Response: <pre>" . xmlHighlight($dom->saveXML()) . "</pre>";
 	exit;
 }
 
-?><form><select name="api"><?php
+?><div style="float: right; width: 75%; height: 100%"><iframe name="results" style="width: 100%; height: 100%"></iframe></div><form target="results"><select name="api"><?php
 foreach ($apis as $api => $params) {
 	?><option value="<?php echo $api; ?>"<?php if ($api == $_GET["api"]) echo ' selected="selected"'; ?>><?php echo $api; ?></option><?php
 }
@@ -80,10 +88,22 @@ foreach ($apis as $api => $params) {
 foreach ($apis as $api => $params) {
 	?><div id="<?php echo $api; ?>"<?php if ($api != $_GET["api"]) echo ' style="display: none"'; ?>><?php
 	foreach ($params as $k => $v) {
-		?><?php echo $k; ?>: <input name="<?php echo $api . $k; ?>" value="<?php echo isset($_GET[$v]) ? htmlspecialchars($_GET[$v]) : htmlspecialchars($v); ?>" /><br/><?php
+		if (is_array($v)) {
+			foreach ($v as $k1 => $v1) {
+				echo $k, "[$k1]"; ?>: <input name="<?php echo $api , $k, "[$k1]"; ?>" value="<?php echo isset($_GET[$k][$k1]) ? htmlspecialchars($_GET[$k][$k1]) : htmlspecialchars($v1); ?>" /><br/><?php
+			}
+		} else {
+			echo $k; ?>: <input name="<?php echo $api, $k; ?>" value="<?php echo isset($_GET[$k]) ? htmlspecialchars($_GET[$k]) : htmlspecialchars($v); ?>" 	/><br/><?php
+		}
 	}
 	?></div><?php
 }
 
 ?><input type="submit" /></form>
-</body></html>
+<script>
+document.getElementsByTagName("select")[0].onchange = function() {
+	var divs = document.getElementsByTagName("div");
+	for (var i = 1, l = divs.length; i < l; i++) divs[i].style.display = "none";
+	document.getElementById(this.value).style.display = "block";
+}
+</script></body></html>
